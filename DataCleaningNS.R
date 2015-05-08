4759c225434094b58b3425595fd99aa8ac771418
Nandini Shah <nss2158@columbia.edu>
2015-05-07 21:38:40 -0400

    with priority stuff done

--- DataCleaningNS.R
+++ DataCleaningNS.R
@@ -4,6 +4,8 @@ library(scales)
 library(dplyr)
 library(leaps) # best subset
 library(glmnet) # lasso
+library(stats) #AIC, BIC
+library(glmnetcr)
 
 ##################################################
 ## INITIAL CLEANING OF GTD DATA ##
@@ -22,11 +24,10 @@ datafull <- read.csv('gtd_06_to_13.csv', header=T, stringsAsFactor = F, na.strin
 # n <- nrow(datafull)
 #data.cols <- names(datafull)
 
-######### filter data for top 6 countries ###########
+######### filter data for top 6 countries = 70% of full dataset (datafull) ###########
 data.top6 <- filter(datafull, country_txt=="Iraq" | country_txt=="Pakistan" | country_txt=="Afghanistan" | 
-                      country_txt=="India" | country_txt=="Philippines" | country_txt=="Thailand") #country_txt=="United States")
+                      country_txt=="India" | country_txt=="Philippines" | country_txt=="Thailand") 
 rm(datafull)
-#@@@ United States versus Thailand
 
 ######### cleaning columns ###########
 
@@ -67,8 +68,6 @@ other.cols <- c("eventid", "provstate", "city","latitude","longitude","specifici
                "dbsource", "target1", "corp1", "nkillter", "nkillus", "nwoundus","nwoundte")
 # remove other columns with text or other information thats not needed
 data.small <- data.small[, !(names(data.small) %in% other.cols)]
-#@@@ What abt "nxxxus" cols, target1, extended etc? 
-#@@@ Removed additionally - "target1", "corp1", "nkillter", "nkillus", "nwoundus","nwoundte"
 
 # last 4 columns contain internation info - check that there is sufficient data filled
 # -9 = unknown - check that there are at least n/2 knowns in these columns
@@ -82,22 +81,12 @@ for (i in 1:length(internat.cols) ){
    }
 }
 #@@@ NS: Want to look at this in more detail.
+#@@@ NS: Deal with -9 in 'property' column
 
 # remove international columns with too many unknowns (-9) rows
 data.small <- data.small[, !(names(data.small) %in% int.exclude)]
 
 ######### vectorizing text columns ###########
-
-#@@@ There's 6k+ of these. Not sure if it makes sense to include. Too much variance, prolly not adding any value.
-# # vectorize corp column from character
-# corp <- data.small$corp1
-# # unique names
-# unique.corp <- unique(data.small$corp1)
-# # match index 
-# corp.index <- match(corp, unique.corp)
-# # add to data.small
-# data.small$corp.index <- corp.index
-
 
 # vectorize gname column 
 gname <- data.small$gname
@@ -115,40 +104,23 @@ rm(gname)
 rm(unique.gname)
 rm(unknown.rows)
 
-#@@@ This might not be a good column to include. Redundant info. If we do use, should normalize it. Removed for now.
-# # vectorize target1 column 
-# target1 <- data.small$target1
-# # unique names
-# unique.target1 <- unique(data.small$target1)
-# # match index 
-# target1.index <- match(target1, unique.target1)
-# # add to data.small
-# data.small$target1.index <- target1.index
-
-#@@@ Combining the response variables: nkill + nwound = nvictim
-nvictim <- data.small$nkill + data.small$nwound
-# removecols <- c()
-# print("here4 first")
-# column <- which(data.small%in%c("nvictim"))
-# for (j in 1:length(dat[,i])){if (is.na(data.small[j,i]) || is.infinite) {removecols <- rbind(removecols, j) }}
-#   dat <- dat[-c(removecols),]
-#   print('nvictim Worked!')
-# }
-data.small$nvictim <- c(log2(nvictim))
+#@@@ Combining the response variables: nkill + nwound = ncasualty
+ncasualty <- data.small$nkill + data.small$nwound
+data.small$ncasualty <- c(log2(ncasualty))
 data.small$nkill <- NULL
 data.small$nwound <- NULL
-rm(nvictim)
+rm(ncasualty)
 
-#@@@ Working with NAs
+#@@@ Working around NAs and Inf - since glmnet does not work with them
 cleaningNAs <- function(dat, i){
-  #"natlty" - assuming country of incidence
+  #"natlty" - assuming country of incidence based on source (GTD) documentation
   if (names(dat)[i] == "natlty1"){
-    #@@@ Has to be a better way to do this than below......
     print('here1')
     column <- which(names(dat)%in%c("country"))
     for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {dat[j,i] = dat[j, column]}}
     print('natlty1 Worked!')
   }
+  #"guncertain1" - deleting the rows since not a significant number
   if (names(dat)[i] == "guncertain1"){
     removecols <- c()
     print("here2")
@@ -156,53 +128,58 @@ cleaningNAs <- function(dat, i){
     dat <- dat[-c(removecols),]
     print('guncertain1 Worked!')
   }
+  #"ishostkid" - deleting the rows since not a significant number
   if (names(dat)[i] == "ishostkid"){
     print("here3")
     for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {dat <- dat[-c(j),]} }
     print('ishostkid Worked!')
   }
-  if (names(dat)[i] == "nvictim"){
+  #"ncasualty" - deleting the rows since this is the response variable
+  if (names(dat)[i] == "ncasualty"){
     removecols <- c()
     print("here4")
     for (j in 1:length(dat[,i])){if (is.na(dat[j,i]) || is.infinite(dat[j,i])) {removecols <- rbind(removecols, j) }}
     dat <- dat[-c(removecols),]
-    print('nvictim Worked!')
+    print('ncasualty Worked!')
   }
-  
-  #@@@ weapsubtype1
+  #"weapsubtype1" - deleting the rows since not a significant number
+  #@@@ NS: implement LR if can
   if (names(dat)[i] == "weapsubtype1"){
     removecols <- c()
     print("here5")
     for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {removecols <- rbind(removecols, j)}}
     dat <- dat[-c(removecols),]
     print('weapsubtype1 Worked!')
-    #fit <- lm(y ~ x1 + x2 + x3, data=mydata)
   }
-  #@@@ nperps and nperpcap columns --- ignoring these cols for now. to figure LR.
-  if (names(dat)[i] == "nperps"){
-    removecols <- c()
-    print("here7")
-    for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {removecols <- rbind(removecols, j)}}
-    dat <- dat[-c(removecols),]
-    i = which(names(dat)%in%c("nperpcap"))
-    for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {removecols <- rbind(removecols, j)}}
-    dat <- dat[-c(removecols),]
-    print('nperps Worked!')
-  }
-#   #@@@ nperps and nperpcap columns --- ignoring these cols for now. to figure LR.
+#   #"nperps" and "nperpcap" columns 
+#   #@@@ NS: try to figure using LR.
 #   if (names(dat)[i] == "nperps"){
 #     removecols <- c()
-#     print("here6")
-#     column <- which(names(dat)%in%c("nperps"))
-#     dat[,column] <- NULL
-#     column <- which(names(dat)%in%c("nperpcap"))
-#     dat[,column] <- NULL
+#     print("here7")
+#     for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {removecols <- rbind(removecols, j)}}
+#     dat <- dat[-c(removecols),]
+#     i = which(names(dat)%in%c("nperpcap"))
+#     for (j in 1:length(dat[,i])){if (is.na(dat[j,i])) {removecols <- rbind(removecols, j)}}
+#     dat <- dat[-c(removecols),]
 #     print('nperps Worked!')
 #   }
+  #"nperps" and "nperpcap" columns --- ignoring these cols for now since metrics prove these are not significant
+  if (names(dat)[i] == "nperps"){
+    removecols <- c()
+    print("here6")
+    column <- which(names(dat)%in%c("nperps"))
+    dat[,column] <- NULL
+    column <- which(names(dat)%in%c("nperpcap"))
+    dat[,column] <- NULL
+    print('nperps Worked!')
+  }
   return(dat)
 }
 
-########## Linear Regression for weapsubtype1 : WIP ################################################################## 
+
+
+
+########## WIP: Linear Regression for weapsubtype1 ################################################################## 
 plot(lowess(x=data.small$weaptype1+data.small$property,y=data.small$weapsubtype1))
 
 fit <- data.frame()
@@ -221,7 +198,7 @@ data.lm.train <- data.lm[train.ndx, ]
 data.lm.test <- data.lm[-train.ndx, ]
 #data.lm.test.labels <- data.lm.labels[-train.ndx, ]
 
-form <- as.formula(sprintf('weapsubtype1 ~ weaptype1 + nvictim + property + guncertain1'))
+form <- as.formula(sprintf('weapsubtype1 ~ weaptype1 + ncasualty + property + guncertain1'))
 model <- lm(form, data=data.lm.train)
 summary(model)
 data.lm.train.labels <- predict(model, data.lm.train)
@@ -235,10 +212,11 @@ ggplot(data=plot.data, aes(x=weaptype1, y=weapsubtype1)) +
   xlab('Age') + ylab('Daily pageviews') +
   theme(legend.title=element_blank(), legend.position=c(0.9,0.85))
 ggsave(filename='./lectures/lecture_4/figures/mean_daily_pageviews_by_age_and_gender.pdf', width=8, height=4)
-
-
 ##################################################################################################################
 
+
+
+#@@@ Removing the NAs by calling the function defined above for each column in the data set
 NAVector <- c()
 flag <- c()
 for (i in 1:length(names(data.small)))
@@ -250,46 +228,65 @@ for (i in 1:length(names(data.small)))
   if (anyNA(data.small[,i])) {
     countbefore = sum(is.na(data.small[,i]))
     ratiobefore = countbefore/length(data.small[,i])
-    data.small <- cleaningNAs(data.small, i)
+    data.small <- cleaningNAs(data.small, i)  #calling the function
     countnow = sum(is.na(data.small[,i]))
     rationow = countnow/length(data.small[,i])
   }
   flag <- c(names(data.small)[i], anyNA(data.small[,i]), ratiobefore, countbefore, rationow, countnow) 
   NAVector <- rbind(NAVector,flag)
 }
-
-
-# # label - success
-# label = data.frame(success = data.small$success, stringsAsFactors = F)
+colnames(NAVector) <- c("feature","anyNA","ratiobefore","countbefore","rationow","countnow")
+  
 
 #@@@ splitting into train/test sets
 set.seed(1010)
 ndx <- sample(nrow(data.small), floor(nrow(data.small) * 0.9))
 train <- data.small[ndx,]
 test <- data.small[-ndx,]
-trainX <- train[!names(test) %in% ("nvictim")]
-trainY <- train[,as.numeric(names(test) %in% ("nvictim"))]
-testX <- test[,!names(test) %in% ("nvictim")]
-testY <- test[,names(test) %in% ("nvictim")]
+trainX <- train[!names(test) %in% c("ncasualty")]
+trainY <- train[names(test) %in% ("ncasualty")]
+testX <- test[,!names(test) %in% c("ncasualty")]
+testY <- test[,names(test) %in% ("ncasualty")]
 rm(train)
 rm(test)
-rm(ndx)
 
-#@@@ Lasso Regression on train
-lambda = 0.01
-#cvob1=glmnet(as.matrix(trainX),trainY, lambda=0.5) # NS: cv = cross-validation; default alpha=1 
-cvob1=cv.glmnet(as.matrix(trainX),trainY) # NS: cv = cross-validation; default alpha=1 
+#@@@ Lasso Regression on train set
+#@@@ NS: To interpret the lambda plots and add to slides
+#@@@ GABY - Feel free to pretty up the graph
+cvob1=cv.glmnet(as.matrix(trainX),as.matrix(trainY),alpha=1,family="gaussian")
 plot(cvob1)
+grid()
 coef(cvob1)
-cvob2=cv.glmnet(as.matrix(trainX),trainY,alpha=0)
-plot(cvob2)
-coef(cvob2)
 
-tfit=glmnet(as.matrix(trainX),trainY,lower=-.7,upper=.5)
-plot(tfit)
+# cvob2=cv.glmnet(as.matrix(trainX),as.matrix(trainY),alpha=0)
+# plot(cvob2)
+# coef(cvob2)
 
-# subset selection
-reg.model <- regsubsets(nvictim ~ ., data = data.small, nvmax = 20)
+#@@@ GABY - Feel free to pretty up the graph
+cvob3=glmnet(as.matrix(trainX),as.matrix(trainY))
+plot(cvob3,label=TRUE)
+grid()
+coef(cvob3)
+
+#@@@ Lasso Regression on test set
+#@@@ NS: Try to plot CI on this
+#testPred <- predict(cvob1,as.matrix(testX))
+#trainPred <- predict(cvob1,as.matrix(trainX))
+RMSE.test <- sqrt(sum((testY-(predict(cvob1,as.matrix(testX))))^2)/length(testY))
+RMSE.train <- sqrt(sum((trainY-(predict(cvob1,as.matrix(trainX))))^2)/nrow(trainY))
+RMSE.test
+RMSE.train
+
+
+#@@@ Logistic on "success" column
+#@@@ NS: To try and adjust weights to adjust for reporting bias
+model <- glm(success ~ ., data=data.small[ndx, ], family="binomial") 
+table(predict(model, data.small[-ndx, ]) > 0, data.small[-ndx, "success"])
+
+
+
+######################## subset selection ##################################
+reg.model <- regsubsets(ncasualty ~ ., data = data.small, nvmax = 26)
 reg.summary <- summary(reg.model)
 
 # point which maximizes adjusted rsquared, Cp and BIC - Best Subset Selection
@@ -297,6 +294,13 @@ max.adjR <- which.max(reg.summary$adjr2)
 min.cp <- which.min(reg.summary$cp) 
 min.bic <- which.min(reg.summary$bic) 
 
+
+# selecting best model based on least rss
+coef(reg.model, min.rss)
+RMSE.test.SS <- sqrt(sum((testY-(predict((reg.model,),as.matrix(testX))))^2)/length(testY))
+RMSE.test <- sqrt(sum((testY-(predict(cvob1,as.matrix(testX))))^2)/length(testY))
+RMSE.train <- sqrt(sum((trainY-(predict(cvob1,as.matrix(trainX))))^2)/nrow(trainY))
+  
 ## plot adjusted r2
 plot(reg.summary$adjr2, xlab = "Size of Subset", ylab = "Adjusted RSq", type ="l", col="blue", main="Best subset selection")
 # add point with max RSq
@@ -304,7 +308,7 @@ points(max.adjR, reg.summary$adjr2[max.adjR], col="red", cex=2, pch=20)
 grid()
 # best model according to Adjusted R squared
 print("Best subset based on Adjusted R^2")
-coef(reg.model, max.adjR)
+#coef(reg.model, max.adjR)
 
 
 ## plot Cp
@@ -315,7 +319,7 @@ grid()
 
 # best model according to CP
 print("Best subset based on Cp")
-coef(reg.model, min.cp)
+#coef(reg.model, min.cp)
 
 
 ## plot BIC
@@ -326,7 +330,7 @@ grid()
 
 # best model according to BIC
 print("Best subset based on BIC")
-coef(reg.model, min.bic)
+#coef(reg.model, min.bic)
 
 
 # plots - which subset to chose?
@@ -351,3 +355,29 @@ filename = "bic_subsets.png"
 dev.copy(device = png, filename = filename) # save png
 dev.off()
 
+# based on BIC 
+plot(reg.model, scale = "rss", main="RSS - subsets", col="red")
+grid()
+filename = "rss_subsets.png"
+dev.copy(device = png, filename = filename) # save png
+dev.off()
+
+#@@@ GABY - lasso selects best model based on trainRSS. To compare apples-to-apples we should do subset selection based on RSS.
+#@@@ plot the RSS for different models 
+(code here)
+#@@@ select best model based on min.RSS
+min.rss <- which.min(reg.summary$rss) 
+#@@@ calculate test RSS, bic, auc, adj r-squared for this model
+(code here)
+
+
+#@@@ VISAL/GABY - Really tried doing this for sometime but it's just so much easier in excel :P If you can figure in R pls do.
+#@@@ Comparing ranks of the coeffs of the two models
+SScoef <- coef(reg.model, min.rss)
+SScoef <- sort(SScoef,decreasing=TRUE)
+Lascoef <- data.frame()
+Lascoef <- rbind(names(data.small))
+Lascoef <- rbind(Lascoef,temp[,1])
+Lascoef <- sort(Lascoef[2,], decreasing=TRUE)
+rank(SScoef)
+rank(Lascoef[2,])
